50ac84633560367d11ae5e56343030a7a4203cc5
Ramzan Ali <purvam98@gmail.com>
2018-05-19 09:40:40 -0400

    every

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegetable Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
